51a1481bd26f95c5ec2b8777029db363c1c5b142
Danyzzz <android.developer@libero.it>
2015-10-25 19:07:10 +0100

    Update ScreenLocker.java

--- src/android/ScreenLocker.java
+++ src/android/ScreenLocker.java
@@ -91,8 +91,7 @@ public class ScreenLocker extends CordovaPlugin {
                     @Override
                     public void run() {
                         PowerManager powerManager = (PowerManager) cordova.getActivity().getSystemService(Context.POWER_SERVICE);
-                        PowerManager.WakeLock wakeLock = powerManager.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK
-                                | PowerManager.FULL_WAKE_LOCK
+                        PowerManager.WakeLock wakeLock = powerManager.newWakeLock((PowerManager.SCREEN_DIM_WAKE_LOCK
                                 | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
                         if(wakeLock.isHeld()) {
                             wakeLock.release();
